bd4866d210e647c9ad30837a1ff3b02bf6c40d72
Can Li <lican81@gmail.com>
2019-06-23 23:56:09 -0700

    Initial commit

---
+++ firmware/A0_DUT.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=A0_DUT.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/A0_DUT.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=a0dut.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/a0dut.x.tar
