7fa10decefd31217080c5aa7ffb46bb2a05514b1
Can Li <lican81@gmail.com>
2019-08-07 12:56:36 -0700

    read_dpe_int function

--- dut_a0.py
+++ dut_a0.py
@@ -400,7 +400,7 @@ def read_dpe_col_int(Vread, Vgate, row, array=0, col=0, gain=0, Vref=0.5):
     VREF_LO = 0.5
 
     dut.scan_control(scan_ctrl_bits=bytes([0x10, 0x02, 0x0c, 0x10,
-                                           0x20, 0x01, 0x02]))
+                                           0x0c, 0x01, 0x02]))
     dut.scan_tia( BitArray(_gain_table[gain]*96).bytes )
     
     # Make sure the VPP is reasonable
@@ -422,9 +422,9 @@ def read_dpe_col_int(Vread, Vgate, row, array=0, col=0, gain=0, Vref=0.5):
 
     dut.reset_dpe()
 
-    # drv.gpio_pin_set(*PIC_PINS['DPE_INTERNAL_EN'])
+    drv.gpio_pin_set(*PIC_PINS['DPE_INTERNAL_EN'])
 
-    drv.gpio_pin_set(*PIC_PINS['DPE_EXT_OVERRIDE_EN'])
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_OVERRIDE_EN'])
     drv.gpio_pin_set(*PIC_PINS['READ_BIT'])
     drv.gpio_pin_set(*PIC_PINS['READ_DPE'])
 
@@ -433,12 +433,12 @@ def read_dpe_col_int(Vread, Vgate, row, array=0, col=0, gain=0, Vref=0.5):
     drv.gpio_pin_set(*PIC_PINS['CONNECT_TIA'])
     drv.gpio_pin_set(*PIC_PINS['CONNECT_COLUMN_T'])
 
-    # drv.gpio_pin_set(*PIC_PINS['DPE_PULSE'])
-    # drv.gpio_pin_reset(*PIC_PINS['DPE_PULSE'])
+    drv.gpio_pin_set(*PIC_PINS['DPE_PULSE'])
+    drv.gpio_pin_reset(*PIC_PINS['DPE_PULSE'])
 
-    drv.gpio_pin_set(*PIC_PINS['DPE_EXT_PULSE'])
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_PULSE'])
     # time.sleep(1e-6)
-    drv.gpio_pin_set(*PIC_PINS['DPE_EXT_SH'])
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_SH'])
     
     [fifo_en, channel] = dut.which_fifo([array, col])
 
@@ -446,3 +446,123 @@ def read_dpe_col_int(Vread, Vgate, row, array=0, col=0, gain=0, Vref=0.5):
     volt = dut.adc2volt(data[channel]) - VREF_LO
     
     return volt / _gain_ratio[gain]
+
+def read_dpe_int(Vread, Vgate, row_vector, array=0, gain=0, Vref=0.5):
+    '''
+    Args,
+
+    Returns:
+        The ADC readout voltages
+    '''
+    VREF_TIA = Vref
+    VREF_LO = 0.5
+
+    dut.scan_control(scan_ctrl_bits=bytes([0x10, 0x02, 0x0c, 0x10,
+                                           0x0c, 0x01, 0x02]))
+    dut.scan_tia( BitArray(_gain_table[gain]*96).bytes )
+    
+    # Make sure the VPP is reasonable
+    assert VREF_TIA - Vread > -0.2 and VREF_TIA - Vread < 1
+
+    dut.dac_set('PLANE_VPP', VREF_TIA - Vread)
+    dut.dac_set('P_VREF_TIA', VREF_TIA)
+    dut.dac_set('P_TVDD', Vgate)
+
+    data_load = dut.data_generate_vector(row_vector, [0xffff,0xffff,0xffff,0xffff])
+    dut.load_vectors(array=array, data=data_load)
+
+    dut.pads_defaults()
+
+    dut.reset_dpe()
+
+    drv.gpio_pin_set(*PIC_PINS['DPE_INTERNAL_EN'])
+
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_OVERRIDE_EN'])
+    drv.gpio_pin_set(*PIC_PINS['READ_BIT'])
+    drv.gpio_pin_reset(*PIC_PINS['READ_DPE'])
+
+    # drv.gpio_nforce_safe_write(0b100)
+    drv.gpio_nforce_safe_write( 0b1 << array )
+    drv.gpio_pin_set(*PIC_PINS['CONNECT_TIA'])
+    drv.gpio_pin_set(*PIC_PINS['CONNECT_COLUMN_T'])
+
+    drv.gpio_pin_set(*PIC_PINS['DPE_PULSE'])
+    drv.gpio_pin_reset(*PIC_PINS['DPE_PULSE'])
+
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_PULSE'])
+    # time.sleep(1e-6)
+    # drv.gpio_pin_set(*PIC_PINS['DPE_EXT_SH'])
+    
+    volt = []
+    fifo_en = [(2-array)*2, (2-array)*2+6, (2-array)*2+1, (2-array)*2+7]
+    data1 = dut.download_fifo( fifo_en[0] )
+    data2 = dut.download_fifo( fifo_en[1] )
+    for b in range(0, 32, 2):
+        channel = (b//16)*8 + (7-b%16//2)
+        volt.append((dut.adc2volt(data1[channel]) - VREF_LO) / _gain_ratio[gain])
+        volt.append((dut.adc2volt(data2[channel]) - VREF_LO) / _gain_ratio[gain])
+    data3 = dut.download_fifo( fifo_en[2] )
+    data4 = dut.download_fifo( fifo_en[3] )
+    for b in range(32, 64, 2):
+        channel = (3-b//16)*8 + b%16//2
+        volt.append((dut.adc2volt(data3[channel]) - VREF_LO) / _gain_ratio[gain])
+        volt.append((dut.adc2volt(data4[channel]) - VREF_LO) / _gain_ratio[gain])
+    
+    # [fifo_en, channel] = dut.which_fifo([array, 3])
+    # data = dut.download_fifo( fifo_en )
+    # volt = dut.adc2volt(data[channel]) - VREF_LO
+
+    return volt
+
+# def read_dpe_allarray_int(Vread, Vgate, row_vector, gain=0, Vref=0.5):
+#     '''
+#     Args,
+
+#     Returns:
+#         The ADC readout voltages
+#     '''
+#     VREF_TIA = Vref
+#     VREF_LO = 0.5
+
+#     dut.scan_control(scan_ctrl_bits=bytes([0x10, 0x02, 0x0c, 0x10,
+#                                            0x20, 0x01, 0x02]))
+#     dut.scan_tia( BitArray(_gain_table[gain]*96).bytes )
+    
+#     # Make sure the VPP is reasonable
+#     assert VREF_TIA - Vread > -0.2 and VREF_TIA - Vread < 1
+
+#     dut.dac_set('PLANE_VPP', VREF_TIA - Vread)
+#     dut.dac_set('P_VREF_TIA', VREF_TIA)
+#     dut.dac_set('P_TVDD', Vgate)
+
+#     data_load = dut.data_generate_vector(row_vector, [0xffff,0xffff,0xffff,0xffff])
+#     dut.load_vectors(array=array, data=data_load)
+
+#     dut.pads_defaults()
+
+#     dut.reset_dpe()
+
+#     # drv.gpio_pin_set(*PIC_PINS['DPE_INTERNAL_EN'])
+
+#     drv.gpio_pin_set(*PIC_PINS['DPE_EXT_OVERRIDE_EN'])
+#     drv.gpio_pin_set(*PIC_PINS['READ_BIT'])
+#     drv.gpio_pin_set(*PIC_PINS['READ_DPE'])
+
+#     # drv.gpio_nforce_safe_write(0b100)
+#     drv.gpio_nforce_safe_write( 0b111 )
+#     drv.gpio_pin_set(*PIC_PINS['CONNECT_TIA'])
+#     drv.gpio_pin_set(*PIC_PINS['CONNECT_COLUMN_T'])
+
+#     # drv.gpio_pin_set(*PIC_PINS['DPE_PULSE'])
+#     # drv.gpio_pin_reset(*PIC_PINS['DPE_PULSE'])
+
+#     drv.gpio_pin_set(*PIC_PINS['DPE_EXT_PULSE'])
+#     # time.sleep(1e-6)
+#     drv.gpio_pin_set(*PIC_PINS['DPE_EXT_SH'])
+    
+#     [fifo_en, channel] = dut.which_fifo([array, col])
+
+#     data = dut.download_fifo( fifo_en )
+#     volt = dut.adc2volt(data[channel]) - VREF_LO
+    
+#     return volt / _gain_ratio[gain]
