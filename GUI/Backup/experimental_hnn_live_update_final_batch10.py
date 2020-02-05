86ad82093103c4a3005af0ecc1d1ca90360af19a
jpstrachan <53101500+jpstrachan@users.noreply.github.com>
2020-02-05 14:33:44 -0800

    JPS changes

---
+++ GUI/Backup/experimental_hnn_live_update_final_batch10.py
@@ -0,0 +1,217 @@
+import sys
+sys.path.append('../')
+
+import pickle
+#import matplotlib
+#matplotlib.rcParams['font.sans-serif'] = "Arial"
+
+import pylab as plt
+import numpy as np
+#from time import sleep
+#import matplotlib
+
+import os
+simulation = (os.environ["USERNAME"].upper()=="VANVAERE")
+
+if not simulation:
+    import serial
+    from dpe import DPE
+    from lib_data import *
+    from lib_nn_dpe import NN_dpe
+
+numCycles = 1
+numTrials = 2
+startSchmidtVal = -1.5
+endSchmidtVal = +0.4
+
+def run_memHNN(numCycles=numCycles,
+               numTrials=numTrials,
+               startSchmidtVal=startSchmidtVal,
+               endSchmidtVal=endSchmidtVal,
+               simulation=simulation,
+               figure_canvas=None,
+               fig=None,
+               show_plot=True,
+               verbosity=0,
+               load_reference_data=True):
+    if verbosity>0.:
+        print("Enter run memHNN function.")
+        print("This is a"+simulation*" simulation"+(not simulation)*("n experiment"))
+    if not simulation:
+        if (os.environ["USERNAME"].upper()=="STRACHAJ"):
+            dpe_inst = DPE('COM5')
+        else:
+            dpe_inst = DPE('COM10')
+        dpe_inst.set_clock(50)
+        dpe_inst.shape
+
+        # fn = "../../20200129-172219-Prober2_HNN_20cyc_1trial_VarSchmidt.pkl"
+        # fn = "./20200130-120058-memHNN_LinearCorrections.pkl"
+        fn = "../20200204-172148-memHNN_LinearCorrections10AtATime_Prober1System.pkl"
+        data_lc = None
+        with open(fn, "rb") as pkl_file:
+            data_lc = pickle.load(pkl_file)
+        lin_corrs = data_lc["lin_corrs10AtATime"]
+
+        import scipy.io as sio
+        mat_contents = sio.loadmat('./Exported60Node_GraphNum0.mat')
+        CMat = mat_contents['A']
+
+        CMat[0, 33] = 0
+        CMat[33, 0] = 0
+        CMat[56, 6] = 0
+        CMat[6, 56] = 0
+        if verbosity>0.:
+            print("Experiment: matrix loaded")
+    else:
+        fn = "./Thomas/20191113-222550-Prober2_HNN_15cyc_100trials_Neg3_0Pos1_4.pkl"
+
+        data = None
+        if verbosity>1.:
+            print("CWD: {}".format(os.getcwd()))
+        with open(fn, "rb") as pkl_file:
+            if verbosity > 1.: print("Pkl file opened.")
+            data = pickle.load(pkl_file)
+            if verbosity>1.: print("Pkl file loaded.")
+        CMat = data["CMat"]
+        if verbosity>0.:
+            print("Simulation: matrix loaded")
+
+
+    arr = 2
+    sizeBatch = 12
+    numBatches = 60/sizeBatch
+    SchmidtCycleVector = np.linspace(startSchmidtVal, endSchmidtVal, numCycles)
+    threshold = 0
+    noise = 0
+    numCols = 60
+    appliedVector1 = np.zeros((64, numTrials))
+    appliedVector2 = np.zeros((64, numTrials))
+    neuronVectorHistory = np.zeros((60, numTrials, numCycles * numCols + 1))
+    neuronVector = np.zeros((60, numTrials))
+    columnUpdateHistory = np.zeros((1, numCycles * numCols + 1))
+    energyHistory = np.zeros((numCycles * numCols + 1, numTrials))
+    num_updates = numCycles * numCols + 1
+    time_vector = np.arange(0, num_updates)
+    color_idx_array = np.linspace(0, 1.0, numTrials)
+
+    #if not simulation:
+    #    lin_corrs = np.zeros_like(appliedVector1)
+
+    # Make the updatable Figure - this code taken from Thomas
+    if figure_canvas==None and show_plot:
+        plt.ion()
+    plt.rcParams['lines.linewidth'] = 2.5  # instead of 1.5
+    color_map = 'summer' #'jet'  # 'cool' #try also 'prism', check here: https://matplotlib.org/examples/color/colormaps_reference.html
+
+    if fig==None:
+        fig = plt.figure(0, figsize=[plt.rcParams["figure.figsize"][0] * 2., plt.rcParams["figure.figsize"][1]])
+    else:
+        fig.clf()
+    ax = fig.add_subplot(111)
+    color_idx_array = np.linspace(0, 1.0, numTrials)
+    ax.set_xlabel("Time", fontsize=15)
+    ax.set_ylabel("Energy", fontsize=15)
+    ax.set_xlim([time_vector[0], time_vector[-1]])
+    ax.set_ylim([-200, 100.])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    trial_index = 0
+    energy_vector = np.NaN * np.zeros((num_updates, numTrials))  # NaNs such that it's not plotted
+
+    legend_list=[]
+    if load_reference_data:
+        energy_vector_nn = np.loadtxt('memhnn_data_nonoise.txt')
+        energy_vector_opt = np.loadtxt('memhnn_data_optimal.txt')
+        time_vector_nn=np.arange(0,energy_vector_nn.shape[0])
+        time_vector_opt = np.arange(0, energy_vector_opt.shape[0])
+        ax.plot(time_vector_nn, energy_vector_nn[:, 0], "-", color="silver", alpha=0.5, linewidth=1.5)
+        ax.plot(time_vector_opt, energy_vector_opt[:, 1], "-", color="gray", alpha=0.5, linewidth=1.5)
+        legend_list+=['No Noise', 'Optimal']
+
+    lines = []
+    for tt in np.arange(numTrials):
+        lobj = ax.plot(time_vector, energy_vector[:,tt], '-', color=plt.get_cmap(color_map)(color_idx_array[tt]))[0]
+        lines.append(lobj)
+
+    # line1, = ax.plot(time_vector, energy_vector[:,0], '-',
+    #                 color=plt.get_cmap(color_map)(color_idx_array[trial_index]))  # Returns a tuple of line objects --> line1,
+    if verbosity>0.:
+        print("Initialize data containers.")
+    thisEnergy = np.zeros(numTrials)
+    for tt in np.arange(numTrials):
+        randomVector = np.random.randint(2, size=(60, 1))
+        initVector = randomVector
+
+        # for ss in np.arange(numSchmidt):
+        appliedVector1[0:60, tt] = initVector[:, 0]
+        appliedVector2[0:60, tt] = 1 - initVector[:, 0]
+        neuronVector[0:60, tt] = appliedVector1[0:60, tt] - appliedVector2[0:60, tt]
+        neuronVectorHistory[:, tt, 0] = appliedVector1[0:60, tt] - appliedVector2[0:60, tt]
+        thisEnergy[tt] = 0.5 * np.dot(neuronVector[:, tt].T, (CMat @ neuronVector[:, tt]))
+        energyHistory[0, tt] = thisEnergy[tt]
+        energy_vector[0, tt] = thisEnergy[tt]
+    if verbosity>0.:
+        print("Run experiment.")
+    for cc in np.arange(numCycles):
+        # print('Cycle number', cc)
+        
+        randOrderColumnBatches = np.arange(numBatches).astype(int)
+        np.random.shuffle(randOrderColumnBatches)
+
+        trackColBatch = 0
+        for ii in randOrderColumnBatches:
+            if not simulation:
+                colStart = np.around(ii*sizeBatch).astype(int)
+                colEnd = np.around((ii+1)*sizeBatch).astype(int)
+                output1 = dpe_inst.multiply_w_delay(arr, appliedVector1, c_sel=[(ii*sizeBatch), ((ii+1)*sizeBatch)],
+                                                    mode=1, debug=False, delay=5)
+                output2 = dpe_inst.multiply_w_delay(arr, appliedVector2, c_sel=[(ii*sizeBatch), ((ii+1)*sizeBatch)],
+                                                    mode=1, debug=False, delay=5)
+                #output_corr = noise - dpe_inst.lin_corr(output1, lin_corrs) + dpe_inst.lin_corr(output2, lin_corrs)
+                output_corr = noise - dpe_inst.lin_corr(output1, lin_corrs[colStart:colEnd]) + dpe_inst.lin_corr(output2, lin_corrs[colStart:colEnd])  
+            else:
+                output_corr = -np.dot(CMat[(ii*sizeBatch):((ii+1)*sizeBatch), :], neuronVector).T
+                #output_corr.shape = (-1, 1)
+            for tt in np.arange(numTrials):
+                threshVector = threshold - SchmidtCycleVector[cc] * neuronVector[ii*sizeBatch:(ii+1)*sizeBatch, tt]
+                # if (output_corr2[0,ii] >= threshVector[ii,0]):
+                # if (output_corr >= threshVector[ii,0]):
+                for columnIndex in np.arange(sizeBatch):
+                    actualColumn = ii*sizeBatch + columnIndex
+                    if (output_corr[tt, columnIndex] >= threshVector[columnIndex]):
+                        appliedVector1[actualColumn, tt] = 1
+                        appliedVector2[actualColumn, tt] = 0
+                        neuronVector[actualColumn, tt] = 1
+                    else:
+                        appliedVector1[actualColumn, tt] = 0
+                        appliedVector2[actualColumn, tt] = 1
+                        neuronVector[actualColumn, tt] = -1
+                    #neuronVectorHistory[:, tt, 60 * cc + trackCol + 1] = neuronVector[:, tt]
+                    thisEnergy[tt] = 0.5 * np.dot(neuronVector[:, tt].T, (CMat @ neuronVector[:, tt]))
+                    # thisEnergy = 0.5*np.dot(neuronVector[:,tt], np.dot(CMat, neuronVector[:,tt]))
+                    #energyHistory[60 * cc + trackCol + 1, tt] = thisEnergy[tt]
+                    energy_vector[60 * cc + trackColBatch*sizeBatch + columnIndex + 1, tt] = thisEnergy[tt]
+                    columnUpdateHistory[0, 60 * cc + trackColBatch*sizeBatch + columnIndex + 1] = actualColumn
+
+                    # line1.set_ydata(energy_vector[:,0])  # wonder whether we can update one point at a time
+            for lnum, line in enumerate(lines):
+                # line.set_data(xlist[lnum], ylist[lnum]) # set data for each line separately.
+                line.set_ydata(energy_vector[:, lnum])  # wonder whether we can update one point at a time
+            if show_plot:
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+                if cc==0:
+                    legend_list+=["User: Trial {}".format(1+lnum) for lnum,lines in enumerate(lines)]
+                    ax.legend(legend_list, bbox_to_anchor=(1.1, 1.1))
+            if figure_canvas!=None and show_plot:
+                figure_canvas.draw()
+
+            
+            trackColBatch = trackColBatch + 1
+
+    return neuronVectorHistory, energy_vector
+
+if (__name__=="__main__"):
+    _, energy_vector = run_memHNN()
---
+++ GUI/Backup/mnist_demo.py
@@ -0,0 +1,382 @@
+
+
+from PyQt5 import QtCore
+from PyQt5 import QtGui, QtWidgets, uic
+import qimage2ndarray
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib
+matplotlib.rcParams['font.sans-serif'] = "Arial"
+
+import numpy as np
+from skimage.transform import resize
+import Arrow_rc
+
+# To Add Image with QT Designer:
+# 1. Create image and save (.jpg, etc.)
+# 2. Make as a resource file in QT designer and place with label (see here: https://stackoverflow.com/questions/28536306/inserting-an-image-in-gui-using-qt-designer)
+# 3. Save file, compile saved resource file on command line "pyrcc5 Arrow.qrc -o Arrow_rc.py" where Arrow.qrc is the name of your saved resource file
+# 4. In .py file, import created compiled .py file, example above is "import Arrow_rc"
+# Note: images can alter spacing, so can be tweaky this way without fering to layoutnames. May change widget spacing.
+# Note: also unknown what file dependencies there are. Hopefully it's local relative to UI file, otherwise may need to reimport and save resource file
+
+# qtCreatorFile = "MNIST_demo.ui"
+qtCreatorFile = "MNIST_demo_V2.ui"
+Ui_MainWindow, QMainWindow = uic.loadUiType(qtCreatorFile)
+
+
+class DrawingWidget(QtWidgets.QWidget):
+    newPoint = QtCore.pyqtSignal(QtCore.QPoint)
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.path = QtGui.QPainterPath()
+        self.pen = QtGui.QPen(QtCore.Qt.black, 20, QtCore.Qt.SolidLine)
+
+        self.clear()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+
+        painter.setPen(self.pen)
+
+        painter.drawPath(self.path)
+
+    def mousePressEvent(self, event):
+        self.path.moveTo(event.pos())
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.path.lineTo(event.pos())
+        self.newPoint.emit(event.pos())
+        self.update()
+
+    def sizeHint(self):
+        return QtCore.QSize(280, 280)
+
+    def clear(self):
+        self.path.clear()
+        self.path.addRect(0, 0, 280, 280)
+        self.update()
+
+    # def save(self):
+    #     pixmap = QtGui.QPixmap(280, 280)
+    #     pixmap.fill(Qt.white)
+
+    #     painter = QtGui.QPainter(pixmap)
+    #     painter.setRenderHint(QtGui.QPainter.Antialiasing)
+    #     painter.setPen(self.pen)
+
+    #     painter.drawPath(self.path)
+    #     painter.end()
+    #     pixmap.save("path.png")
+
+    #     self.clear()
+
+    def toArray(self):
+        pixmap = QtGui.QPixmap(280, 280)
+        pixmap.fill(QtCore.Qt.white)
+
+        painter = QtGui.QPainter(pixmap)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.setPen(self.pen)
+
+        painter.drawPath(self.path)
+        painter.end()
+
+        arr = qimage2ndarray.rgb_view(pixmap.toImage())
+        return arr
+
+
+class MnistMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, ):
+        super(MnistMainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.mpl_digit.hide()
+
+        self.drawing = DrawingWidget(parent=self.drawing_digit)
+        self.btn_clear.clicked.connect(self.drawing.clear)
+
+        # Setup the digit panel
+        self.fig_digit = Figure(figsize=(3,3))
+        # self.ax_digit = self.fig_digit.add_subplot(111)
+        self.ax_digit = self.fig_digit.add_axes((0,0,1,1))
+        self.ax_digit.get_xaxis().set_visible(False)
+        self.ax_digit.get_yaxis().set_visible(False)
+        self.canvas_digit = FigureCanvas(self.fig_digit)
+        self.canvas_digit.setParent(self.mpl_digit)
+
+        # Setup convolution weight panel
+        self.fig_conv_weight = Figure(figsize=(2,3))
+        # self.ax_conv_weight = self.fig_conv_weight.add_axes((0,0,1,1))
+        # self.ax_conv_weight.get_xaxis().set_visible(False)
+        # self.ax_conv_weight.get_yaxis().set_visible(False)
+        self.canvas_conv_weight = FigureCanvas(self.fig_conv_weight)
+        self.canvas_conv_weight.setParent(self.mpl_conv_weight)
+
+        # Setup FC weight panel
+        self.fig_fc_weight = Figure(figsize=(2,4))
+        # self.ax_fc_weight = self.fig_fc_weight.add_axes((0,0,1,1))
+        # self.ax_fc_weight.get_xaxis().set_visible(False)
+        # self.ax_fc_weight.get_yaxis().set_visible(False)
+        self.canvas_fc_weight = FigureCanvas(self.fig_fc_weight)
+        self.canvas_fc_weight.setParent(self.mpl_fc_weight)
+
+        # Setup conv input panel
+
+        self.fig_conv_in = Figure(figsize=(1.5,3))
+        # self.ax_conv_in = self.fig_conv_in.add_axes((0,0,1,1))
+        # self.ax_conv_in.get_xaxis().set_visible(False)
+        # self.ax_conv_in.get_yaxis().set_visible(False)
+        self.canvas_conv_in = FigureCanvas(self.fig_conv_in)
+        self.canvas_conv_in.setParent(self.mpl_conv_in)
+
+        # Setup conv output panel
+        self.fig_conv_out = Figure(figsize=(1.7,3))
+        self.ax_conv_outs = [None] * 7
+        for i in range(7):
+            self.ax_conv_outs[i] = self.fig_conv_out.add_subplot(4,2,i+1)
+            self.ax_conv_outs[i].get_xaxis().set_visible(False)
+            self.ax_conv_outs[i].get_yaxis().set_visible(False)
+
+        self.canvas_conv_out = FigureCanvas(self.fig_conv_out)
+        self.canvas_conv_out.setParent(self.mpl_conv_out)
+        self.fig_conv_out.tight_layout()
+
+
+        # Setup FC output panel
+        self.fig_fc_out = Figure(figsize=(6,3))
+        # self.ax_fc_out = self.fig_fc_out.add_axes((0,0,1,1))
+        self.ax_fc_out = self.fig_fc_out.add_subplot(111)
+        self.canvas_fc_out = FigureCanvas(self.fig_fc_out)
+        self.canvas_fc_out.setParent(self.mpl_fc_out)
+        self.fig_fc_out.tight_layout()
+
+
+
+        # Connect button click signals
+        self.btn_classify.clicked.connect(self.classify)
+        self.btn_read.clicked.connect(self.read_conductance)
+
+        self._init_dpe()
+
+    def _init_dpe(self):
+        self._conf = {
+            'r_conv':   20,
+            'c_conv':   20,
+            'arr_conv': 0,
+            'r_fc':     0,
+            'c_fc':     0,
+            'arr_fc': 1,
+        }
+
+        self.dpe = DPE('COM3')
+        self.dpe.set_clock(50)
+
+        load_workspace(self._conf, '../20200130-100802-mnist_config')
+
+        # load_workspace(self._conf, '../20200130-105530-mnist_config_prober1')
+        # print(self._conf['arr_conv'])
+    
+        self.nn = NN_dpe(self._conf['weights'])
+
+
+    def read_conductance(self):
+        self.statusbar.showMessage('Reading conductance...')
+        self.repaint()
+
+        g_conv = self.dpe.read(self._conf['arr_conv'], method='fast')
+        g_conv = g_conv[self._conf['r_conv']:self._conf['r_conv']+26,
+                        self._conf['c_conv']:self._conf['c_conv']+14, ]
+
+        self.fig_conv_weight.clf()
+        # self.ax_conv_weight = self.fig_conv_weight.add_axes((0,0,1,1))
+        self.ax_conv_weight = self.fig_conv_weight.add_subplot(111)
+        self.ax_conv_weight.get_xaxis().set_visible(False)
+        self.ax_conv_weight.get_yaxis().set_visible(False)
+        self.ax_conv_weight.set_title('Conductance ($\mu$S)')
+        im_conv = self.ax_conv_weight.imshow(g_conv * 1e6)
+        self.ax_conv_weight.set_aspect('auto')
+        self.fig_conv_weight.colorbar(im_conv, ax=self.ax_conv_weight)
+        self.fig_conv_weight.tight_layout()
+        self.canvas_conv_weight.draw()
+
+        g_fc = self.dpe.read(self._conf['arr_fc'], method='fast')
+        g_fc1 = g_fc[self._conf['r_fc']:self._conf['r_fc']+57,
+                     self._conf['c_fc']:self._conf['c_fc']+20, ]
+
+        g_fc2 = g_fc[self._conf['r_fc']:self._conf['r_fc']+56,
+                     self._conf['c_fc']+20:self._conf['c_fc']+40, ]
+
+        g_fc = np.concatenate((g_fc1, g_fc2), axis=0)
+
+        self.fig_fc_weight.clf()
+        self.ax_fc_weight = self.fig_fc_weight.add_subplot(111)
+        self.ax_fc_weight.get_xaxis().set_visible(False)
+        self.ax_fc_weight.get_yaxis().set_visible(False)
+        self.ax_fc_weight.set_title('Conductance( $\mu$S)')
+        im_fc = self.ax_fc_weight.imshow(g_fc * 1e6)
+        self.ax_fc_weight.set_aspect('auto')
+        self.fig_fc_weight.colorbar(im_fc, ax=self.ax_fc_weight)
+        self.fig_fc_weight.tight_layout()
+        self.canvas_fc_weight.draw()
+
+        self.statusbar.showMessage('Conductance read completed.')
+
+
+    def classify(self):
+        for i in range(7):
+            self.ax_conv_outs[i].cla()
+        self.canvas_conv_out.draw()
+        self.ax_fc_out.cla()
+        self.canvas_fc_out.draw()
+
+        img = self.drawing.toArray()
+        img = self._pre_process(img)
+        
+        # self.ax_digit.imshow( img )
+        # self.canvas_digit.draw()
+        # self.repaint()
+
+        ## Convolution layer
+        image = img[..., np.newaxis]
+        vectors = self.nn._conv_flattern(image)
+
+        self._disp_conv_in(vectors)
+
+        print('Convolving image...')
+
+        output = self.dpe.multiply(
+            self._conf['arr_conv'], 
+            vectors, 
+            c_sel=[self._conf['c_conv'], self._conf['c_conv']+14], 
+            r_start=self._conf['r_conv'], mode=0, Tdly=500) / (self.nn.Gratio/2)
+
+        output_cor = self.dpe.lin_corr(output, self._conf['lin_cor_conv'])
+        x = output_cor[:, ::2] - output_cor[:, 1::2]
+        x = x.reshape(20, 20, -1)
+
+        self._disp_conv_out(x)
+        self.repaint()
+
+        print('Convolution completed, start fully connected layer...')
+
+        ## Fully connected layer
+        x1 = self.nn.relu(x)
+        x1 = self.nn.max_pooling(x1)
+        x1 = self.nn.flattern(x1)
+
+        x = x1
+        x1 = x[:57].T
+        x2 = x[57:].T
+
+        sc1 = x1.max()
+        sc2 = x2.max()
+
+        # Gfc1 = nn.Gfc[:57]
+        # Gfc2 = nn.Gfc[57:]
+
+        x1 = x1 / sc1
+        x2 = x2 / sc2
+
+        output1 = self.dpe.multiply(self._conf['arr_fc'], np.array([x1]).T, c_sel=[
+                            0, 20], mode=0, Tdly=500)
+        output1 = self.dpe.lin_corr(output1, self._conf['new_lin_cor_fc1']) * sc1
+
+        output2 = self.dpe.multiply(self._conf['arr_fc'], np.array([x2]).T, c_sel=[
+                            20, 40], mode=0, Tdly=500)
+        output2 = self.dpe.lin_corr(output2, self._conf['new_lin_cor_fc2']) * sc2
+
+        outputs = output1 + output2
+
+        y = outputs[:, ::2] - outputs[:, 1::2]
+        
+        self._plot_result(y)
+
+        print(f'Classfied digit {y.argmax()}.')
+
+    def _disp_conv_in(self, img):
+        self.fig_conv_in.clf()
+        self.ax_conv_in = self.fig_conv_in.add_subplot(111)
+
+        self.ax_conv_in.set_title('Voltage (V)')
+        self.ax_conv_in.get_xaxis().set_visible(False)
+        self.ax_conv_in.get_yaxis().set_visible(False)
+        im_conv_in = self.ax_conv_in.imshow(img.T* 0.2)
+        self.ax_conv_in.set_aspect('auto')
+        self.fig_conv_in.colorbar(im_conv_in, ax=self.ax_conv_in)
+        self.fig_conv_in.tight_layout()
+
+        self.canvas_conv_in.draw()
+        self.repaint()
+
+        # self.ax_conv_in.imshow(img.T)
+        # self.canvas_conv_in.draw()
+        # self.repaint()
+
+
+
+    def _disp_conv_out(self, x):
+        for i in range(7):
+            self.ax_conv_outs[i].imshow(x[:,:,i])
+        self.fig_conv_out.tight_layout()
+        self.canvas_conv_out.draw()
+
+    def _plot_result(self, y):
+        digit = y.argmax()
+        ys = self._result_softmax(y.reshape(-1))
+
+        self.ax_fc_out.cla()
+        self.ax_fc_out.bar(range(10), ys, color='grey' )
+        self.ax_fc_out.bar([digit], ys[digit], color='green' )
+        self.ax_fc_out.set_ylabel('Probability)')
+        # self.ax_fc_out.set_ylabel('Average current ($\mu$A))')
+        # self.ax_fc_out.set_title(f'Recognized {y.argmax()}')
+        self.ax_fc_out.set_xticks(np.arange(0,10,1))
+        self.ax_fc_out.grid(True, alpha=0.3)
+        self.fig_fc_out.tight_layout()
+        self.canvas_fc_out.draw()
+        self.repaint()
+
+        self.label_result.setText(f'Recognized : {digit}')
+
+    def _result_current(self, x):
+        return x / 256 * 1e6
+
+    def _result_softmax(self, x):
+        x = x.astype(np.double) * 1e3
+        exps = np.exp(x - np.max(x))
+        return exps / np.sum(exps)
+
+    def _result_prob(self, x):
+        return (x - x.min()) / sum(x - x.min())
+
+    def _pre_process(self, img):
+        padding = 10
+
+        img = np.mean(img, axis=2).astype(np.float) / 255
+        img = img[padding:-padding, padding:-padding]
+
+        img = resize(img, (24, 24), anti_aliasing=True)
+        img = 1-img
+
+        return img
+
+
+if __name__ == '__main__':
+    import sys
+    sys.path.insert(0, '..')
+
+    from lib_data import *
+    from lib_nn_dpe import NN_dpe
+    from dpe import DPE
+
+    app = QtWidgets.QApplication(sys.argv)
+    main = MnistMainWindow()
+
+
+    main.show()
+    sys.exit(app.exec_())
